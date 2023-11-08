fbce487e30697abf28ad064074a0468f445795ae
Luong <luongvqph33534@fpt.edu.vn>
2023-11-08 17:07:17 +0700

    fix db

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App.js";
-import { QuizProvider } from "./contexts/QuizContext.js";
+// import { QuizProvider } from "./contexts/QuizContext.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <QuizProvider>
-      <App />
-    </QuizProvider>
+    {/* <QuizProvider> */}
+    <App />
+    {/* </QuizProvider> */}
   </React.StrictMode>
 );
